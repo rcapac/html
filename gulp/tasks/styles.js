74f0789aaad592a484a011a24689d76461740b86
rcapac <robcapac@gmail.com>
2018-04-21 01:17:22 -0400

    responsive images for hero title

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,12 +1,17 @@
-gulp = require('gulp'),
-postcss = require('gulp-postcss'),
-autoprefixer = require('autoprefixer'),
-cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested'),
-cssImport = require('postcss-import');
+var gulp = require('gulp'),
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
+    cssvars = require('postcss-simple-vars'),
+    nested = require('postcss-nested'),
+    mixins = require('postcss-mixins'),
+    cssImport = require('postcss-import');
 
 gulp.task('styles', function() {
     return gulp.src('./app/assets/styles/styles.css')
-           .pipe(postcss([cssImport, nested, cssvars, autoprefixer]))
+           .pipe(postcss([cssImport, mixins, nested, cssvars, autoprefixer]))
+           .on('error', function(errorInfo) {
+               console.log(errorInfo.toString());
+               this.emit('end');
+           })
            .pipe(gulp.dest('./app/temp/styles'));
 });
