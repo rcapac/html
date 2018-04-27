2ea35bff6750aec0b2ded225cf3e2b369e7ab32b
rcapac <robcapac@gmail.com>
2018-04-26 20:59:39 -0400

    Completed reveal scroll behavior

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,3 +1,6 @@
 import MobileMenu from './modules/mobilemenu';
+import RevealOnScroll from './modules/revealScroll';
 
 var mobileMenu = new MobileMenu();
+new RevealOnScroll(".feat-item", "85%");
+new RevealOnScroll(".testimonial", "60%");
