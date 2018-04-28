caf656da6489cc62a950a5896905d9e7041ab3d5
rcapac <robcapac@gmail.com>
2018-04-28 14:20:21 -0400

    Completed modal

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -2,8 +2,10 @@ import MobileMenu from './modules/mobilemenu';
 import RevealOnScroll from './modules/revealScroll';
 import StickyHeader from './modules/StickyHeader';
 import smoothScroll from 'jquery-smooth-scroll';
+import Modal from './modules/Modal';
 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll(".feat-item", "85%");
 new RevealOnScroll(".testimonial", "60%");
 var stickyHeader = new StickyHeader();
+var modal = new Modal();
